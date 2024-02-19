7a544f4fc699003dfd0c8db5bb17c5f214506fd9
David <dawidshevnya@gmail.com>
2024-02-19 12:26:00 +0200

    Connected postgresql instead sqlite3

--- suxnix/settings.py
+++ suxnix/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'suxnix.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'qwerty',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
